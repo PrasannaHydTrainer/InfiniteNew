223b8aaedb2733db1b54a5be5597f5fe4871d9e0
PrasananTrainer03 <PrasananTrainer03>
2022-09-26 12:51:33 +0530

    sql

---
+++ Day6/Employ_Table_New_Working.sql
@@ -0,0 +1,24 @@
+use infinite;
+
+Create Table Employ
+(
+    Empno int AUTO_INCREMENT,
+    Name varchar(50), 
+    Dept varchar(30),
+    Desig varchar(30),
+    Basic INT,
+    primary key(Empno)
+);
+
+
+
+Insert into Employ(Name,Dept,Desig,Basic) values
+('kiran kumar kailasam','java','developer',42233),
+('Vishnu Vardhan Reddy','dotnet','programmer',42133),
+('Ram Kishan','java','developer',82233),
+('Kareem','dotnet','programmer',52234),
+('Sheik Sulthan Mohammad','dotnet','developer',42555),
+('Vaibhav Singh','java','Manager',82552),
+('Rashmi Jadhav','Sql','DBA',42345),
+('Vishnu Priya','Sql','Expert',56662)
+
