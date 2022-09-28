a12a032709d63f2677eaadfb754af1f1c47bfe41
PrasananTrainer03 <PrasananTrainer03>
2022-09-28 10:51:02 +0530

    code

---
+++ Day8/BankDAO.java
@@ -0,0 +1,48 @@
+package com.infinite.bank;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class BankDAO {
+
+	Connection connection;
+	PreparedStatement pst;
+	
+	public String createAccount(Bank bank) 
+			throws ClassNotFoundException, SQLException {
+		int accountNo = generateAccountNo();
+		bank.setAccountNo(accountNo);
+		String cmd = "insert into Bank(AccountNo, FirstName, "
+				+ "LastName, city,State,Amount, CheqFacil,"
+				+ "AccountType) values(?,?,?,?,?,?,?,?)";
+		connection = ConnectionHelper.getConnection();
+		pst = connection.prepareStatement(cmd);
+		pst.setInt(1, accountNo);
+		pst.setString(2, bank.getFirstName());
+		pst.setString(3, bank.getLastName());
+		pst.setString(4, bank.getCity());
+		pst.setString(5, bank.getState());
+		pst.setInt(6, bank.getAmount());
+		pst.setString(7, bank.getCheqFacil());
+		pst.setString(8, bank.getAccountType());
+		pst.executeUpdate();
+		return "Account Created...";
+	}
+	
+	public int generateAccountNo() 
+			throws ClassNotFoundException, SQLException {
+		connection = ConnectionHelper.getConnection();
+		String cmd = "select case when max(accountNo) is NULL"
+				+ " then 1 else max(accountNo)+1 end accno "
+				+ "from Bank";
+		pst = connection.prepareStatement(cmd);
+		ResultSet rs = pst.executeQuery();
+		rs.next();
+		int accno = rs.getInt("accno");
+		return accno;
+	}
+}
+
+
