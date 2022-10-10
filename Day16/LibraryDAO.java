c1fd69830f02bf687552f05e1fdd53613056c4b6
PrasananTrainer03 <PrasananTrainer03>
2022-10-10 11:26:01 +0530

    code

---
+++ Day16/LibraryDAO.java
@@ -0,0 +1,59 @@
+package com.infinite.library;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class LibraryDAO {
+
+	public List<Books> searchBooks(String searchType, String searchValue) throws ClassNotFoundException, SQLException {
+		String sql;
+		boolean isValid=true;
+		if(searchType.equals("id")) {
+			sql = " SELECT * FROM Books WHERE Id = ? " ;
+		} else if(searchType.equals("bookname")) {
+			sql = " SELECT * FROM Books WHERE Name = ? " ;
+		} else if(searchType.equals("authorname")) {
+			sql = " SELECT * FROM Books WHERE Author = ? " ;
+		} else if(searchType.equals("dept")) {
+			sql = " SELECT * FROM Books WHERE Dept = ? " ;
+		}
+		else {
+			isValid=false;
+			sql = " SELECT * FROM Books" ;
+		}
+		Connection connection = ConnectionHelper.getConnection();
+		PreparedStatement pst = connection.prepareStatement(sql);
+		if (isValid==true) {
+			pst.setString(1, searchValue);
+		} 
+		ResultSet rs = pst.executeQuery();
+		Books books = null;
+		List<Books> booksList = new ArrayList<Books>();
+		while(rs.next()) {
+			books = new Books();
+			books.setId(rs.getInt("id"));
+			books.setName(rs.getString("name"));
+			books.setAuthor(rs.getString("author"));
+			books.setEdition(rs.getString("edition"));
+			books.setDept(rs.getString("dept"));
+			books.setNoOfBooks(rs.getInt("TotalBooks"));
+			booksList.add(books);
+		}
+		return booksList;
+	}
+	public int authenticate(String user, String password) throws ClassNotFoundException, SQLException {
+		Connection connection = ConnectionHelper.getConnection();
+		String cmd="select count(*) cnt from libusers where UserName=?  and Password = ?";
+		PreparedStatement pst=connection.prepareStatement(cmd);
+		pst.setString(1,user);
+		pst.setString(2,password);
+		ResultSet rs=pst.executeQuery();
+		rs.next();
+		int count = rs.getInt("cnt");
+		return count;
+	}
+}
