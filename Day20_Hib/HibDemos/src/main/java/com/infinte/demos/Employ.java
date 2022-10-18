b40290e79ff4fd79b9ffde70a262266139e760ee
PrasananTrainer03 <PrasananTrainer03>
2022-10-18 10:28:25 +0530

    code

---
+++ Day20_Hib/HibDemos/src/main/java/com/infinte/demos/Employ.java
@@ -0,0 +1,65 @@
+package com.infinte.demos;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="Employ")
+public class Employ {
+
+	@Id
+	@Column(name="empno")
+	private int empno;
+
+	@Column(name="name")
+	private String name;
+	
+	@Column(name="dept")
+	private String dept;
+	
+	@Column(name="desig")
+	private String desig;
+	
+	@Column(name="basic")
+	private int basic;
+	
+	public int getEmpno() {
+		return empno;
+	}
+	public void setEmpno(int empno) {
+		this.empno = empno;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDept() {
+		return dept;
+	}
+	public void setDept(String dept) {
+		this.dept = dept;
+	}
+	public String getDesig() {
+		return desig;
+	}
+	public void setDesig(String desig) {
+		this.desig = desig;
+	}
+	public int getBasic() {
+		return basic;
+	}
+	public void setBasic(int basic) {
+		this.basic = basic;
+	}
+	@Override
+	public String toString() {
+		return "Employ [empno=" + empno + ", name=" + name + ", dept=" + dept + ", desig=" + desig + ", basic=" + basic
+				+ "]";
+	}
+	
+	
+}
